43d55b683b676ba8a3edeb9054fdddc695038936
Justin <justin@homeceuconnection.com>
2018-02-14 09:46:05 -0600

    removed redundancy from notification listener

--- src/lib/Service/NotificationListener.ts
+++ src/lib/Service/NotificationListener.ts
@@ -1,9 +1,9 @@
-import {EventEmitter, Injectable} from "@angular/core";
+import {EventEmitter, Injectable}    from "@angular/core";
 import {ParsedNotificationStructure} from "../Structure/ParsedNotificationStructure";
-import {NotificationAlert} from "./NotificationAlert";
-import {NotificationApi} from "./NotificationApi";
-import {Observable} from 'rxjs/Observable';
-import {UnsubscribeAll} from "@ng-app-framework/core";
+import {NotificationAlert}           from "./NotificationAlert";
+import {NotificationApi}             from "./NotificationApi";
+import {Observable}                  from 'rxjs/Observable';
+import {UnsubscribeAll}              from "@ng-app-framework/core";
 
 @Injectable()
 export class NotificationListener {
@@ -16,16 +16,18 @@ export class NotificationListener {
         return true;
     };
 
-    constructor(public alert: NotificationAlert,
-                public api: NotificationApi) {
+    constructor(
+        public alert: NotificationAlert,
+        public api: NotificationApi
+    ) {
 
     }
 
     getNotifications() {
-        this.getUnread$()
-            .catch(err => Observable.empty())
-            .finally(() => this.getNotificationsLater())
-            .subscribe();
+        Observable.interval(this.intervalInMilliseconds)
+                  .takeUntil(UnsubscribeAll.merge(this.stopListening))
+                  .flatMap(() => this.getUnread$())
+                  .subscribe();
     }
 
 
@@ -34,14 +36,7 @@ export class NotificationListener {
             return Observable.empty();
         }
         return this.api.getUnread()
-            .catch(err => Observable.empty())
-            .do((notification:ParsedNotificationStructure) => this.alert.notify(notification));
-    }
-
-    getNotificationsLater() {
-        Observable.interval(this.intervalInMilliseconds)
-            .first()
-            .takeUntil(UnsubscribeAll.merge(this.stopListening))
-            .subscribe(() => this.getNotifications());
+                   .catch(err => Observable.empty())
+                   .do((notification: ParsedNotificationStructure) => this.alert.notify(notification));
     }
 }
